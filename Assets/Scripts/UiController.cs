1fe281a7de1f2a31f20f5bbf9cefaf668feb56c2
Miggy Bagadiong <miggy.0825@gmail.com>
2023-06-05 19:37:09 +0800

    AudioManager, Start Menu, and SFX (still WIP)
    AudioManager, Start Menu, and SFX (still WIP)
    > AudioManager.cs
    - Manages the different audios
    - Has 2 sound types: for BGM and SFX
    - Has 2 Audiosources [each for BGM and SFX]
    
    > Sound.cs
    - Contains the Sound Name and AudioClip
    
    > MainMenuManager.cs
    - Controls the canvases for Main Menu
    
    > UiController.cs
    - Contains the controls for Ui Buttons
    - Will move to the UiManager and remove this after move
    
    > StartMenu Scene
    - Contains the Ui for start
    - Contains: Play, Settings (WIP for Pause Menu), Credits, Quit
    
    > Pause Menu Canvas
    - Contains three buttons: Resume, Settings, Quit

---
+++ Assets/Scripts/UiController.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class UiController : MonoBehaviour
+{
+    // will reimplement the stuff here to the UiManager
+    // move this and place in one region
+
+    //private UnityEvent 
+
+    private AudioManager audioMgr;
+
+    // [Header("Pause Screen Sliders")]
+    // public Slider pauseMusicSlider;
+    // public Slider pauseSfxSlider;
+
+    [Header ("Settings Screen Sliders")]
+    public Slider settingsMusicSlider;
+    public Slider settingsSfxSlider;
+
+    void Start()
+    {
+        audioMgr = SingletonManager.Get<AudioManager>();
+
+        // initialize the values of the sliders to 0.7
+        audioMgr.AdjustMusicVolume(0.7f);
+        audioMgr.AdjustSFXVolume(0.7f);
+
+    }
+
+    public void ToggleMusic()
+    {
+        audioMgr.ToggleMusic();
+    }
+
+    public void ToggleSFX()
+    {
+        audioMgr.ToggleSFX();
+    }
+
+    // Sliders Adjusting
+    public void AdjustMusicVolume()
+    {
+        //audioMgr.AdjustMusicVolume(pauseMusicSlider.value);
+        audioMgr.AdjustMusicVolume(settingsMusicSlider.value);
+    }
+
+    public void AdjustSFXVolume()
+    {
+        //audioMgr.AdjustSFXVolume(pauseSfxSlider.value);
+        audioMgr.AdjustSFXVolume(settingsSfxSlider.value);
+    }
+
+    public void OnButtonClick()
+    {
+        audioMgr.PlaySFX("Button Click");
+    }
+
+    public void OnSFXSliderReleased()
+    {
+        // insert a good sfx for this part
+        audioMgr.PlaySFX("Button Click");
+    }
+
+    
+}
